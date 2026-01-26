8ddb89b4fdd08c4a2a36cc1b5aae09756149a3d3
Hemanth Kumar B <165136617+hemanthkumarb485@users.noreply.github.com>
2026-01-26 17:42:24 +0530

    Time: 1 ms (34.9%), Space: 43.1 MB (62.54%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,31 +1,8 @@
 class Solution {
-    public int lengthOfLastWord(String s){
-        String[] arr = s.split(" ");
-        int n = arr.length;
-        String a = arr[n-1];
-        return a.length();
+    public int lengthOfLastWord(String s) {
+        String[] ch = s.split(" ");
+        int n = ch.length;
+        // String a = ch[n-1];
+        return ch[n-1].length();
     }
 }
-
-// class Solution {
-//     public int lengthOfLastWord(String s) {
-//         int c = 0;
-//         int i = s.length() - 1;
-//         while(i >= 0 && s.charAt(i) == ' '){
-//             i--;
-//         }
-//         while(i >= 0 && s.charAt(i) != ' '){
-//             c++;
-//             i--;
-//         }
-//         return c;
-//     }
-// }
-
-// class Solution {
-//     public int lengthOfLastWord(String s) {
-//        String[] arr = s.split(" ");
-//        return arr[arr.length - 1].length();
-//     }
-// }
-
