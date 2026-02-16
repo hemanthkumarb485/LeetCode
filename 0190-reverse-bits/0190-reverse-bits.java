38835324a757a99167eb01ea53c9f5989d92b2a2
Hemanth Kumar B <165136617+hemanthkumarb485@users.noreply.github.com>
2026-02-16 11:33:00 +0530

    Time: 1 ms (60.34%), Space: 42.3 MB (93.42%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,5 @@
 class Solution {
     public int reverseBits(int n) {
-        String a = Integer.toBinaryString(n);
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<32-a.length();i++){
-            sb.append('0');
-        }
-        sb.append(a);
-        sb.reverse();
-        String z = sb.toString();
-        int c = Integer.parseInt(z,2);
-        return c;
+        return Integer.reverse(n);
     }
 }
